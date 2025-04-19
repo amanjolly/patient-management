2b7c48d54c56b47b06048690b85e60bb9ab1af4e
Aman Jolly <42204610+amanjolly@users.noreply.github.com>
2025-04-19 14:18:52 +0530

    Billing Service Client Updated

--- patient-service/src/test/java/com/pm/patientservice/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/pm/patientservice/PatientServiceApplicationTests.java
@@ -1,13 +1,30 @@
 package com.pm.patientservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class PatientServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void contextLoads() {
+    }
+    /*@Test
+    void getPatient() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(
+                "/patientservice/getPatient"
+        ).accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+    }*/
 
 }
